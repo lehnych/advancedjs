e818eebc1e5e6550c0377b2f32c18c8eb7cb520e
a.lehn <Lenina1587@2101>
2021-07-26 09:22:19 +0200

    lesson-1

--- lesson-1/js/main.js
+++ lesson-1/js/main.js
@@ -1,12 +1,12 @@
 'use strict';
 const products = [
-    {id: 1, title: 'Notebook', price: 1000},
-    {id: 2, title: 'Mouse', price: 100},
-    {id: 3, title: 'Keyboard', price: 250},
-    {id: 4, title: 'Gamepad', price: 150},
+    {id: 1, img:'img/product/product-m-01-thumb.jpg', title: 'Notebook', price: 1000, currency: '$'},
+    {id: 2, img:'img/product/product-m-02-thumb.jpg', title: 'Mouse', price: 100, currency: '$'},
+    {id: 3, img:'img/product/product-m-03-thumb.jpg', title: 'Keyboard', price: 250, currency: '$'},
+    {id: 4, img:'img/product/product-m-04-thumb.jpg', title: 'Gamepad', price: 150, currency: '$'},
 ];
 
-const renderProduct = (title, price) => {
+/*const renderProduct = (title, price) => {
     return `<div class="product-item">
                 <div class="product-item__wrapper">
                     <div class="product-item__picture">
@@ -28,12 +28,27 @@ const renderProducts = (list) => {
     const productList = list.map((item) => {
         return renderProduct(item.title, item.price);
     }).join('');
-
-
-
-
-    //console.log(productList);
     document.querySelector('.product-grid__wrapper').innerHTML = productList;
-}
+}*/
+
+const renderProduct = item => `
+    <div id="${item.id}" class="product-item">
+        <div class="product-item__wrapper">
+            <div class="product-item__picture">
+                <a class="btn__add-to-cart" href="#" title="${item.title}"><span class="btn btn__ghost--negative btn--transparent"><i class="icon__cart"></i>Add to Cart</span></a>
+                <picture><img class="img--fluid" src="${item.img}" alt="${item.title}"/></picture>
+            </div>
+            <div class="product-item__body">
+                <div class="product-item__decription">
+                    <h4><a href="xxxx" title="${item.title}">${item.title}</a></h4>
+                </div>
+                <div class="product-item__price"><span class="price--discount">${item.currency}${item.price}</span></div>
+            </div>
+        </div>
+      </div>
+`;
+const renderProducts = list => {
+    document.querySelector('.product-grid__wrapper').insertAdjacentHTML('afterbegin', list.map(item => renderProduct(item)).join(''));
+};
 
 renderProducts(products);
